b19e13f8913b1b71ad9694f0f70f9916b89dda40
je-Rebecca <67889991+je-Rebecca@users.noreply.github.com>
2021-05-11 05:46:41 -0400

    updating app with shop page & collection preview comoonent

---
+++ src/pages/shop/shop.components.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SHOP_DATA from './shop.data.js';
+import CollectionPreviw from '../../components/preview-collection/preview-collection.components';
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreviw key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
